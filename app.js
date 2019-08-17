e8afc66b865d501612ad9911548c63eb93279fdc
Alexander Hoinville <ahoin001@gmail.com>
2019-08-17 13:21:21 -0400

    Add Passport functionality

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 // Require Session 
 const session = require("express-session");
@@ -18,7 +18,7 @@ const LocalStrategy = require("passport-local").Strategy;
 
 
 mongoose
-  .connect('mongodb://localhost/project-folder', {useNewUrlParser: true})
+  .connect('mongodb://localhost/project-folder', { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -37,14 +37,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -63,7 +100,7 @@ const index = require('./routes/index');
 app.use('/', index);
 
 // import routes from auth.js
-app.use('/',require('./routes/auth-routes'));
+app.use('/', require('./routes/auth-routes'));
 
 
 module.exports = app;
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const router = express.Router();
 
+// To allow use for passport
+const passport = require("passport");
+
 // Import User model
 const User = require("../models/user");
 
@@ -59,5 +62,17 @@ router.post("/signup", (req, res, next) => {
             next(error)
         })
 });
+
+router.get("/login", (req, res, next) => {
+    res.render("auth/login");
+});
+
+// The post will contain the passport functionality
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+}));
 
 module.exports = router;
